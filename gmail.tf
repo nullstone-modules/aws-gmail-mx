65512ee88ff81fcd5752f53d47d4c4c91fa412d7
Brad Sickles <brad.sickles@gmail.com>
2020-09-02 08:54:13 -0400

    Trying different approach

--- gmail.tf
+++ gmail.tf
@@ -1,25 +1,18 @@
 locals {
-  mx = {
-    "ASPMX.L.GOOGLE.COM" : 1,
-    "ALT1.ASPMX.L.GOOGLE.COM" : 5,
-    "ALT2.ASPMX.L.GOOGLE.COM" : 5,
-    "ALT3.ASPMX.L.GOOGLE.COM" : 10,
-    "ALT4.ASPMX.L.GOOGLE.COM" : 10,
-  }
+  records = [
+    "1 ASPMX.L.GOOGLE.COM",
+    "5 ALT1.ASPMX.L.GOOGLE.COM",
+    "5 ALT2.ASPMX.L.GOOGLE.COM",
+    "10 ALT3.ASPMX.L.GOOGLE.COM",
+    "10 ALT4.ASPMX.L.GOOGLE.COM",
+  ]
 }
 
-resource "aws_route53_record" "main" {
+resource "aws_route53_record" "this" {
   name    = ""
   type    = "MX"
   zone_id = data.terraform_remote_state.domain.outputs.zone_id
   ttl     = 3600
 
-  for_each = local.mx
-
-  weighted_routing_policy {
-    weight = each.value
-  }
-
-  set_identifier = local.full_name
-  records        = [each.key]
+  records = local.records
 }
