4f1b81473a2bc44b6797db20357345de428c9ac3
Brad Sickles <brad.sickles@gmail.com>
2020-09-02 08:39:10 -0400

    another attempt

--- gmail.tf
+++ gmail.tf
@@ -1,14 +1,24 @@
+locals {
+  mx = {
+    "ASPMX.L.GOOGLE.COM" : 1,
+    "ALT1.ASPMX.L.GOOGLE.COM" : 5,
+    "ALT2.ASPMX.L.GOOGLE.COM" : 5,
+    "ALT3.ASPMX.L.GOOGLE.COM" : 10,
+    "ALT4.ASPMX.L.GOOGLE.COM" : 10,
+  }
+}
+
 resource "aws_route53_record" "main" {
   name    = ""
   type    = "MX"
   zone_id = data.terraform_remote_state.domain.outputs.zone_id
   ttl     = 3600
 
-  for_each = var.mx-records
+  for_each = local.mx
 
   weighted_routing_policy {
-    weight = each.value.weight
+    weight = each.value
   }
 
-  records = [each.value.record]
+  records = [each.key]
 }
