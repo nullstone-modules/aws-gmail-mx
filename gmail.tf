2eb44288afb7f61358fc8c9a6eee51ece3dda83b
Brad Sickles <brad.sickles@gmail.com>
2020-09-02 08:34:39 -0400

    Fixing syntax

--- gmail.tf
+++ gmail.tf
@@ -1,35 +1,10 @@
-locals {
-  records = [
-    {
-      weight = 1,
-      record = "ASPMX.L.GOOGLE.COM",
-    },
-    {
-      weight = 5,
-      record = "ALT1.ASPMX.L.GOOGLE.COM",
-    },
-    {
-      weight = 5,
-      record = "ALT2.ASPMX.L.GOOGLE.COM"
-    },
-    {
-      weight = 10,
-      record = "ALT3.ASPMX.L.GOOGLE.COM"
-    },
-    {
-      weight = 10,
-      record = "ALT4.ASPMX.L.GOOGLE.COM"
-    },
-  ]
-}
-
 resource "aws_route53_record" "main" {
   name    = ""
   type    = "MX"
   zone_id = data.terraform_remote_state.domain.outputs.zone_id
   ttl     = 3600
 
-  for_each = local.records
+  for_each = var.mx-records
 
   weighted_routing_policy {
     weight = each.value.weight
--- variables.tf
+++ variables.tf
@@ -28,3 +28,34 @@ variable "backend_conn_str" {
 locals {
   full_name = "${var.stack_name}-${var.env}-${var.block_name}"
 }
+
+
+variable "mx-records" {
+  type = list(object({
+    weight: number
+    record: string
+  }))
+
+  default = [
+    {
+      weight = 1,
+      record = "ASPMX.L.GOOGLE.COM",
+    },
+    {
+      weight = 5,
+      record = "ALT1.ASPMX.L.GOOGLE.COM",
+    },
+    {
+      weight = 5,
+      record = "ALT2.ASPMX.L.GOOGLE.COM"
+    },
+    {
+      weight = 10,
+      record = "ALT3.ASPMX.L.GOOGLE.COM"
+    },
+    {
+      weight = 10,
+      record = "ALT4.ASPMX.L.GOOGLE.COM"
+    },
+  ]
+}
