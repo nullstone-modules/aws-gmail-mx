7f5b9417881a51e795cc2f0ba238e079a4af4930
Brad Sickles <brad.sickles@gmail.com>
2021-05-10 10:58:44 -0400

    Added _dmarc TXT record

--- spf.tf
+++ spf.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "spf" {
+  zone_id = data.ns_connection.domain.outputs.zone_id
   name    = ""
   type    = "TXT"
-  zone_id = data.ns_connection.domain.outputs.zone_id
   ttl     = 3600
   records = ["v=spf1 include:_spf.google.com ~all"]
 }
