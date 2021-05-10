4a9d9a3cea313d878e269ab18daadf96c5b6bb5b
Brad Sickles <brad.sickles@gmail.com>
2021-05-10 10:31:10 -0400

    Chunked dkim txt record in strings 255 chars long

--- dkim.tf
+++ dkim.tf
@@ -1,9 +1,19 @@
+locals {
+  // AWS does not allow TXT records longer than 255
+  // To get around this, you must specify a chunk of 255 with each quoted double-quoted
+  // Per Terraform docs, the record is already double-quote
+  //   As a result, you must place 2 double-quotes between each chunk
+  dkim_chars  = chunklist(split("", try(var.dkim.value, "")), 255)
+  dkim_chunks = [for chunk in local.dkim_chars : join("", chunk)]
+  dkim_record = join("\"\"", local.dkim_chunks)
+}
+
 resource "aws_route53_record" "dkim" {
   name    = var.dkim.name
   type    = "TXT"
   zone_id = data.ns_connection.domain.outputs.zone_id
   ttl     = 3600
-  records = [var.dkim.value]
+  records = [local.dkim_record]
 
-  count = var.dkim != null && length(var.dkim.value) > 0 ? 1 : 0
+  count = length(local.dkim_record) > 0 ? 1 : 0
 }
